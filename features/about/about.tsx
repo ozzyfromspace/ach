aceb46d5d2ec38238a74d9e5d535c257c730c79b
ozzyfromspace <float.circuit@gmail.com>
2022-11-02 23:02:49 +0000

    create about page

--- features/about/about.tsx
+++ features/about/about.tsx
@@ -5,34 +5,37 @@ import Padding from '../padding';
 const About = () => {
   return (
     <Padding>
-      <section className="w-full bg-purple-300" id="about">
-        <h2 className="text-[1.4rem] sm:text-2xl md:text-3xl lg:text-4xl font-medium">
+      <section className="w-full" id="about">
+        <h2 className="text-[1.6rem] sm:text-[1.8rem] md:text-[2.2rem] lg:text-[2.5rem] font-medium text-center">
           About us
         </h2>
-        <div className="">
-          <div>
-            <p>
-              The Athens Central Hotel is a hidden gem in the heart of Athens,
-              Ohio.
-            </p>
-            <p>
+        <div className="md:flex md:flex-row md:flex-wrap md:justify-center md:items-end xl:flex-row xl:items-center">
+          <div className="mt-8 text-justify md:min-w-[30%] md:max-w-[50%] lg:max-w-screen-lg xl:max-w-screen-lg md:pr-8 lg:pr-0 md:flex md:flex-col md:justify-center lg:flex-row lg:flex-wrap">
+            <p className="pb-3 md:pb-6 lg:w-1/3 xl:max-w-[33%] lg:pr-8 lg:pb-0">
+              <span className="font-semibold">
+                The Athens Central Hotel is a hidden gem in the heart of Athens,
+                Ohio.
+              </span>{' '}
               Located only a few steps from Court street, we{"'"}re incredibly
               close to all the nightlife, restaurants, and vibrant community.
+            </p>
+            <p className="pb-3 md:pb-6 lg:w-1/3 xl:max-w-[33%] lg:pr-8 lg:pb-0">
               Our hotel is newly built with a modern European aesthetic. Every
               room in the hotel is designed uniquely with our guests in mind. We
               make sure to go above and beyond to give our guests the best
               experience and make sure they have a wonderful stay.
             </p>
-            <p>
+            <p className="lg:w-1/3 xl:max-w-[34%]">
               From our convenient location to the exquisite rooms, upgraded
               showers, complimentary weekend breakfast, and free onsite parking,
               you{"'"}ll enjoy your stay every time.
             </p>
           </div>
-          <div>
+          <div className="relative mt-9 md:min-w-[10rem] md:w-[50%] lg:w-[65%] xl:max-w-screen-xl xl:mx-auto">
             <Image
               src={beds}
               alt="two beds from a queen room of the athens central hotel"
+              className="rounded-md xl:rounded-lg brightness-[115%] saturate-[1.33]"
             />
           </div>
         </div>
