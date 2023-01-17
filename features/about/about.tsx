bc772fdf47467a4cd6b123ceb40f424ec134190b
ozzyfromspace <float.circuit@gmail.com>
2023-01-17 15:47:54 -0500

    add padding to textual content of about/contact section

--- features/about/about.tsx
+++ features/about/about.tsx
@@ -29,7 +29,7 @@ const About = (props: AboutProps) => {
       <div className="w-fit mx-auto" id="contact" ref={aboutRef}>
         <div
           id="contact-content"
-          className={`mt-16 flex flex-wrap justify-between items-start gap-6 mt:gap-9 md:gap-11 lg:gap-14 w-fit mx-auto`}
+          className={`mt-2 flex flex-wrap justify-between items-start gap-6 mt:gap-9 md:gap-11 lg:gap-14 w-fit mx-auto pt-4 px-6`}
         >
           <div className="mx-auto space-y-11">
             <Address />
@@ -66,16 +66,16 @@ const About = (props: AboutProps) => {
               src="https://badge.hotelstatic.com/?position=inline&amp;size=120&amp;clickable=false&amp;url=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fus%2Fathens-central.html"
             ></iframe>
           </div>
-          <Padding className="select-none mx-auto w-full mt:w-[min(89vh,89vw)] max-w-lg">
-            <iframe
-              aria-label="A google map showing the Athens Central Hotel"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3086.0494872241065!2d-82.09823380000003!3d39.33246430000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8848735b1021f905%3A0x400f2aa6ca675df3!2sAthens%20Central%20Hotel!5e0!3m2!1sen!2sus!4v1672186189896!5m2!1sen!2sus"
-              className="border-[1px] p-[1px] border-white w-full aspect-square rounded-md shadow-sm"
-              allowFullScreen={true}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </Padding>
+          {/* <Padding className="select-none mx-auto w-full mt:w-[min(89vh,89vw)] max-w-lg"> */}
+          <iframe
+            aria-label="A google map showing the Athens Central Hotel"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3086.0494872241065!2d-82.09823380000003!3d39.33246430000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8848735b1021f905%3A0x400f2aa6ca675df3!2sAthens%20Central%20Hotel!5e0!3m2!1sen!2sus!4v1672186189896!5m2!1sen!2sus"
+            className="border-[1px] p-[1px] select-none mx-auto w-full mt:w-[min(89vh,89vw)] max-w-lg border-white aspect-square rounded-md shadow-sm"
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          {/* </Padding> */}
         </div>
         <Footer />
       </div>
