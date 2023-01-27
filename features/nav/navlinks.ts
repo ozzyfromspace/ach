3352337f8a6525832c923b92ca730becceb47ece
ozzyfromspace <float.circuit@gmail.com>
2023-01-27 12:14:19 -0500

    fix scroll-points for links on nav in desktop and mobile

--- features/nav/navlinks.ts
+++ features/nav/navlinks.ts
@@ -1,7 +1,7 @@
 export const navlinks = [
-  { route: 'hero', label: 'Home' },
-  { route: 'rooms', label: 'Rooms' },
-  { route: 'amenities', label: 'Amenities' },
-  { route: 'events', label: 'Events' },
-  { route: 'contact', label: 'Contact' },
+  { route: 'hero', label: 'Home', landmark: 'hero' },
+  { route: 'rooms', label: 'Rooms', landmark: 'rooms-content' },
+  { route: 'amenities', label: 'Amenities', landmark: 'amenities-content' },
+  { route: 'events', label: 'Events', landmark: 'events-content' },
+  { route: 'contact', label: 'Contact', landmark: 'contact-content' },
 ] as const;
